531789a68bcc3253e622dbbc9e4d913a80fb1a42
HyeonWooJung <1995hyunwoo@gmail.com>
2022-01-31 22:20:22 +0900

    Refactor: AppConfig 적용 -> 관심사의 분리 ->  DIP / OCP 원칙 적용

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,12 @@ package hello.core.member;
 // interface에 대한 구현체가 하나면 구현체 되에 Impl이라는 접미사를 붙이는게 관례
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    /** 추상*/
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,20 +1,20 @@
 package hello.core.order;
-
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
     /** 구현체인 FixDiscountPolicy와 RateDiscountPolicy에 의존하고 있음! -> DIP 원칙 위반!!!!
      * interface인 DiscountPlicy에 의존은 하지만, 위와 같은 문제가 있다    * */
-    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy1) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy1;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
