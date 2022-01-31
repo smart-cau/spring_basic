97afdc9dbd89897773f01e65167faf3a74bec33d
HyeonWooJung <1995hyunwoo@gmail.com>
2022-01-31 20:34:54 +0900

    Feat: 비율 할인정책 적용 및 DIP 위반 발건

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,11 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    /** 구현체인 FixDiscountPolicy와 RateDiscountPolicy에 의존하고 있음! -> DIP 원칙 위반!!!!
+     * interface인 DiscountPlicy에 의존은 하지만, 위와 같은 문제가 있다    * */
+    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
