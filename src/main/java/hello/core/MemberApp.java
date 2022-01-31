fbabf1ef2d6e115edcb8038a66e81eceeaecf50d
HyeonWooJung <1995hyunwoo@gmail.com>
2022-02-01 00:25:50 +0900

    Refactor: Spring Container 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,18 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // Spring은 모든게 ApplicationContext로 시작함. Spring Container임. @Bean이 있는 모든 클래스를 관리함
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // AppConfig에 있는 @Bean 붙은 클래스들을 Spring Container에 넣어 관리함
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "member A", Grade.VIP);
         memberService.join(member);
 
