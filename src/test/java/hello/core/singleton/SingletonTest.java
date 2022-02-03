386df3717015851502afdca1f7e7e23bc4fcc0e9
HyeonWooJung <1995hyunwoo@gmail.com>
2022-02-03 14:17:47 +0900

    Feat: Singleton > Singleton Pattern과 Singleton Container 예제

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +24,39 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("Singleton Pattern을 적용한 객체 사용")
+    void singletonServiceTest() {
+        // private으로 생성자를 막아둠. 컴파일 오류 발생
+        // new SingletonService()
+        // 1. 조화: 호출할 때마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        // 2. 조화: 호출할 때마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+        // 참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // same(==)은 memory 값 비교, equals()는 value를 비교함
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("Spring Container & Singleton")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 1. 조화: 호출할 때마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        // 2. 조화: 호출할 때마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+
     }
 }
