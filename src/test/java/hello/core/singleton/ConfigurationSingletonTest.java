01cee2fdc8048ba7ca7b4ba0cc34ffdaf31c7ed5
HyeonWooJung <1995hyunwoo@gmail.com>
2022-02-03 15:01:42 +0900

    Feat: Singleton > Singleton을 보장해주는 @Configuration

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -28,4 +28,17 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // AppConfig도 Spring Bean으로 등록된다
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        // 출력 -> bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$38410218 -> " CGLIB " 이 있음!
+        // 순수한 클래스라면 다음과 같이 출력되어야 함 -> class hello.core.AppConfig
+        // AppConfig가 아니라, AppConfig@CGLIB이 Spring Bean으로 등록됨!!
+    }
 }
